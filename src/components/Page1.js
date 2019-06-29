5601be36863e64fb633d8b9519a05304746b600f
Computer <gabecadiz@hotmail.com>
2019-06-29 14:23:32 -0400

    added button styling and disabled button component to codesplitting example project

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -8,6 +8,7 @@ const Page1 = ({ onRouteChange }) => {
         <h1>Page 1</h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <button className="disabled">Page1</button>
       <button onClick={() => onRouteChange('page2')}>Page2</button>
       <button onClick={() => onRouteChange('page3')}>Page3</button>
     </div>
