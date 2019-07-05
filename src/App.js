c7bdae7158972e79332ff9b4dca4a2d0337afb42
Computer <gabecadiz@hotmail.com>
2019-07-05 14:51:50 -0400

    created initial code spltting for 3 pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Page1 from './components/Page1'
-import Page2 from './components/Page2'
-import Page3 from './components/Page3'
 
-const App = () => {
-  const [route, setRoute] = useState('page1');
-
-  const onRouteChange = (route) => {
-    setRoute(route)
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1',
+      component: ''
+    }
   }
 
-  if (route === 'page1') {
-    return <Page1 onRouteChange={onRouteChange} />
-  } else if (route === 'page2') {
-    return <Page2 onRouteChange={onRouteChange} />
-  } else if (route === 'page3') {
-    return <Page3 onRouteChange={onRouteChange} />
+  onRouteChange = (route) => {
+    if (route === 'page1') {
+      this.setState({ route: route })
+    } else if (route === 'page2') {
+      //asynchronouse code, returns a promise
+      import('./components/Page2').then((Page2) => {
+        //.default is from Webpack
+        this.setState({ route: route, component: Page2.default })
+      })
+    } else if (route === 'page3') {
+      import('./components/Page3').then((Page3) => {
+        this.setState({ route: route, component: Page3.default })
+      })
+    }
   }
+  render() {
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />
+    } else {
+      return <this.state.component onRouteChange={this.onRouteChange} />
+    }
+  }
+
+
+  // if (route === 'page1') {
+  //   return <Page1 onRouteChange={onRouteChange} />
+  // } else if (route === 'page2') {
+  //   return <Page2 onRouteChange={onRouteChange} />
+  // } else if (route === 'page3') {
+  //   return <Page3 onRouteChange={onRouteChange} />
+  // }
+
 
 }
 
